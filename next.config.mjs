fd5295ec70b8ca3bf475fb219f789de7768bac23
banana18nt@gmail.com <banana18nt@gmail.com>
2025-04-22 17:43:40 +0700

    it ok

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    cpus: 2, // Số lượng CPUs bạn muốn giới hạn
+},
+  reactStrictMode: true,
+};
+
+export default nextConfig;
